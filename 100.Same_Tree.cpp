ec1500b8895cb97ecad9c9484ba26751e7d8ecf6
kris-lee <kris-lee@pku.edu.cn>
2016-12-14 22:17:06 +0800

    class Solution {

---
+++ 100.Same_Tree.cpp
@@ -0,0 +1,16 @@
+	/*
+	100. Same Tree
+	Given two binary trees, write a function to check if they are equal or not.
+	Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+	*/
+class Solution {
+public:
+	bool isSameTree(TreeNode* p, TreeNode* q) {
+		if (p == NULL ^ q == NULL) return false;
+		if (p == NULL && q == NULL) return true;
+		else {
+			if(p->val != q->val) return false;
+			else return isSameTree(p->left,q->left) && isSameTree(p->right, q->right);
+		}
+    }
+}
