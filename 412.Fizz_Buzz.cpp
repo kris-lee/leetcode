193166035ddd44ee141e4e3ae83a442162693327
kris-lee <kris-lee@pku.edu.cn>
2016-12-14 19:26:17 +0800

    leetcode 412.Fizz_Buzz

---
+++ 412.Fizz_Buzz.cpp
@@ -0,0 +1,24 @@
+//412. Fizz Buzz
+//Write a program that outputs the string representation of numbers from 1 to n.
+//But for multiples of three it should output ¡°Fizz¡± instead of the number and for the multiples of five output		¡°Buzz¡±. 
+//For numbers which are multiples of both three and five output ¡°FizzBuzz¡±.
+
+class Solution {
+public:   
+	vector<string> fizzBuzz(int n) {
+		vector<string> output(n);
+		for (int i = 1; i <= n; i++)
+		{
+			if ((i % 3 == 0) && (i % 5 == 0))
+				output[i-1] = "FizzBuzz";
+			else if (i % 3 == 0)
+				output[i-1] = "Fizz";
+			else if (i % 5 == 0)
+				output[i-1] = "Buzz";
+			else
+				output[i-1] = to_string(i);
+		
+		}
+		return output;
+    }
+}
