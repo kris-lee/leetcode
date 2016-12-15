9e5ad9f77cb578b2eaedd3bfe8118329d9705eed
kris-lee <kris-lee@pku.edu.cn>
2016-12-15 21:43:48 +0800

    Leetcode 100.Same_Tree

--- 100.Same_Tree.cpp
+++ 100.Same_Tree.cpp
@@ -1,8 +1,8 @@
-	/*
-	100. Same Tree
-	Given two binary trees, write a function to check if they are equal or not.
-	Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
-	*/
+/*
+100. Same Tree
+Given two binary trees, write a function to check if they are equal or not.
+Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+*/
 class Solution {
 public:
 	bool isSameTree(TreeNode* p, TreeNode* q) {
