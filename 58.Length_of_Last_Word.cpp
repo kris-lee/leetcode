54d383b96d7afe1b49f71c5ff52c17a8f4a5f5a7
kris-lee <kris-lee@pku.edu.cn>
2017-01-04 20:25:08 +0800

    judge null and none-null

---
+++ 58.Length_of_Last_Word.cpp
@@ -0,0 +1,20 @@
+/*
+58. Length of Last Word
+Given a string s consists of upper/lower-case alphabets and empty space characters ' ', 
+return the length of last word in the string.
+If the last word does not exist, return 0.
+Note: A word is defined as a character sequence consists of non-space characters only.
+For example, Given s = "Hello World", return 5.
+*/
+class Solution {
+public:
+	int lengthOfLastWord(string s) {
+		if (s.empty()) return 0;
+		int i = s.size() - 1;
+		while (i >=0 && s[i] == ' ') --i;
+		if (i < 0) return 0;
+		int j = i -1;
+		while (j >= 0 && s[j] != ' ') --j;
+		return i-j;
+    }
+};
