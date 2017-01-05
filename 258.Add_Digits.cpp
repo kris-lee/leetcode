3eab2f2edb97c2c81a16a895940a7a128f3be0d6
kris-lee <kris-lee@pku.edu.cn>
2017-01-06 04:22:41 +0800

    get some examples and find the rules

---
+++ 258.Add_Digits.cpp
@@ -0,0 +1,10 @@
+//258. Add Digits
+//Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+//For example:Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+class Solution {
+public:
+	int addDigits(int num) {
+        if (num == 0) return 0;
+        return num % 9 == 0 ? 9 : num  % 9;
+	}
+};
