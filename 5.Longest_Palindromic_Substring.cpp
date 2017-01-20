f21d33d7944e4521b5bedf161b5983cec51e63e3
kris-lee <kris-lee@pku.edu.cn>
2017-01-20 19:53:00 +0800

    common solution to finish

--- 5.Longest_Palindromic_Substring.cpp
+++ 5.Longest_Palindromic_Substring.cpp
@@ -1,10 +1,10 @@
-	/*
-	5. Longest Palindromic Substring
-	Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
-	Example:	Input: "babad"	Output: "bab"
-	Note: "aba" is also a valid answer.
-	Example:	Input: "cbbd"	Output: "bb"
-	*/
+/*
+5. Longest Palindromic Substring
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+Example:	Input: "babad"	Output: "bab"
+Note: "aba" is also a valid answer.
+Example:	Input: "cbbd"	Output: "bb"
+*/
 class Solution {
 public:
 	bool isStringPalindrome(string s, int front, int rear) {
