9eb1301ef06dbd7ded6f1cb4096005f9af2f9884
kris-lee <kris-lee@pku.edu.cn>
2017-01-20 19:20:12 +0800

    use extra space N space and learn the good solution

---
+++ 229.Majority_Element_II.cpp
@@ -0,0 +1,16 @@
+﻿/*
+229. Majority Element II
+Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times. 
+The algorithm should run in linear time and in O(1) space.
+*/
+class Solution{
+public:
+	vector<int> majorityElementII(vector<int>& nums) {
+		map<int,int> match;
+		vector<int> result;
+		for (int i = 0; i < nums.size(); i++) match[nums[i]]++;
+		for (map<int,int>::iterator it = match.begin(); it != match.end(); it++)
+			if (it->second > nums.size()/3) result.push_back(it->second);
+		return result;	
+		}
+};
